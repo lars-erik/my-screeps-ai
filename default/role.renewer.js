70af65c2ceb400c9893f494b53cf445b6c20a51a
Lars-Erik Aabech <lea@markedspartner.no>
2016-08-16 15:55:58 +0200

    Update role.renewer.js

--- default/role.renewer.js
+++ default/role.renewer.js
@@ -1,5 +1,5 @@
 module.exports = {
-    function run(creep) {
+    run: function(creep) {
         if (creep.room.mainSpawn().renewCreep(creep) !== OK) {
             creep.moveTo(creep.room.mainSpawn());
         }
