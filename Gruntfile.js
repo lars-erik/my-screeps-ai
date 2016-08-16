08f2376f3fc68b8f29b070379e250e54357b6691
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-08-16 23:15:36 +0200

    don't merge grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,12 @@
         copy: {
             main: {
                 files: [
-                    { expand: true, src: ["default/*"], flatten: true, dest: "C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/dropoff-dibs" }
+                    { expand: true, src: ["default/*"], flatten: true, dest: "C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/default" }
                 ]
             },
             game: {
                 files: [
-                    { expand: true, src: ["C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/dropoff-dibs/*"], flatten: true, dest: "default" }
+                    { expand: true, src: ["C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/Sim 2/*"], flatten: true, dest: "default" }
                 ]
             }
         },
@@ -21,7 +21,7 @@
                 tasks: ["copy:main"]
             },
             fromGame: {
-                files: ["C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/dropoff-dibs/*"],
+                files: ["C:/Users/Lars-Erik/AppData/Local/Screeps/scripts/screeps.com/default/*"],
                 tasks: ["copy:game"]
             }
         }
