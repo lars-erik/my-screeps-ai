9caed0bbfa5f82a4df50b9c99b55bff7fe096ca0
Lars-Erik Aabech <lea@markedspartner.no>
2016-08-16 16:14:28 +0200

    Update role.renewer.js

--- default/role.renewer.js
+++ default/role.renewer.js
@@ -2,7 +2,7 @@ module.exports = {
     run: function(creep) {
         creep.say("renewing");
         creep.memory.renewing = true;
-        creep.transfer(RESOURCE_ENERGY, creep.room.mainSpawn());
+        creep.transfer(creep.room.mainSpawn(), RESOURCE_ENERGY);
         if (creep.room.mainSpawn().renewCreep(creep) !== OK) {
             creep.say("go to renew");
             creep.moveTo(creep.room.mainSpawn());
