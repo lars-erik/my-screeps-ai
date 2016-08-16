6884602ab1f13eac2e96b450c53732b03a8fe031
Lars-Erik Aabech <lea@markedspartner.no>
2016-08-16 13:03:40 +0200

    idandpos

--- default/extensions.roomobject.js
+++ default/extensions.roomobject.js
@@ -2,3 +2,7 @@ RoomObject.prototype.closestSource = function () {
     return this.pos.findClosestByPath(FIND_SOURCES,{filter: (source) => source.energy > 0 });
 };
 
+RoomObject.prototype.idAndPos = function () {
+    return this.id + "(" + this.pos.x + "," + this.pos.y + ")";
+};
+
