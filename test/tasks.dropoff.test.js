2c8af734f93395272450ebd5c3bdbb855ac49638
Lars-Erik Aabech <lea@markedspartner.no>
2018-08-26 12:27:30 +0200

    test to check creep moving towards dropoff (incomplete)

--- test/tasks.dropoff.test.js
+++ test/tasks.dropoff.test.js
@@ -1,18 +1,22 @@
 describe("Dropping off", () => {
     const world = require("./world");
     const tasks = require("./../default/tasks");
-    let source = null;
+    let source = null,
+        creep = null,
+        task = null;
 
     beforeEach(() => {
         world.initSimple();
+        Game.spawns.Spawn1.pos = new RoomPosition(5, 5);
+
+        creep = new Creep("x", [MOVE,WORK,CARRY], {room:Game.rooms.W0N0});
+        task = new tasks.dropoff(creep, {});
+        creep.pos = new RoomPosition();
     })
 
     it("adds incoming transaction to closest dropoff point", () => {
-        let creep = new Creep("x", [MOVE,WORK,CARRY], {room:Game.rooms.W0N0});
-        let task = new tasks.dropoff(creep, {});
-        creep.pos = new RoomPosition();
-        creep.pos.findClosestByPath = jest.fn();
         creep.carry.energy = 50;
+        creep.pos.findClosestByPath = jest.fn();
         Game.spawns.Spawn1.addTransaction = jest.fn();
 
         creep.pos.findClosestByPath.mockReturnValue(Game.spawns.Spawn1);
@@ -23,5 +27,17 @@ describe("Dropping off", () => {
             amount: 50,
             from: "x"
         }));
+    });
+
+    it("moves towards closest dropoff point", () => {
+        creep.pos.findClosestByPath = jest.fn();
+        creep.carry.energy = 50;
+        Game.spawns.Spawn1.addTransaction = jest.fn();
+        creep.pos.findClosestByPath.mockReturnValue(Game.spawns.Spawn1);
+        creep.moveTo = jest.fn();
+
+        task.run();
+        
+        expect(creep.moveTo).toHaveBeenCalledWith(5, 5);
     });
 });
