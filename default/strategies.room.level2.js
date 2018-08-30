0c44001ca1acbce005c35946e5a25ae1e7fe8ed7
Lars-Erik Aabech <lea@markedspartner.no>
2018-08-30 16:42:05 +0200

    Update strategies.room.level2.js
    FFS, should push down when not working.

--- default/strategies.room.level2.js
+++ default/strategies.room.level2.js
@@ -3,46 +3,64 @@ function countCreeps(creeps, role) {
 }
 
 const MaxBuilders = 1;
+const lvl1 = require("./strategies.room.level1");
 
-function buildExtensions(room) {
-    let extensionCount = room.find(FIND_STRUCTURES, {filter:{structureType:STRUCTURE_EXTENSION}}).length;
-    let constructionCount = room.find(FIND_MY_CONSTRUCTION_SITES).length;
-    let spawn = room.find(FIND_STRUCTURES, {filter: {structureType:STRUCTURE_SPAWN}})[0];
-    if (extensionCount + constructionCount < 5) {
-        let tries = 0;
-        let oks = 0;
-        for (let y = -2; y <= 2 && oks < 5; y += 2) {
-            for (let x = -2; x <= 2 && oks < 5; x += 2) {
-                let px = spawn.pos.x + x;
-                let py = spawn.pos.y + y;
-                let result = room.createConstructionSite(px, py, STRUCTURE_EXTENSION);
+let level2 = {
+
+    buildExtensions(room) {
+        let extensionCount = room.find(FIND_STRUCTURES, {filter:{structureType:STRUCTURE_EXTENSION}}).length;
+        let constructionCount = room.find(FIND_MY_CONSTRUCTION_SITES).length;
+        let spawn = room.find(FIND_STRUCTURES, {filter: {structureType:STRUCTURE_SPAWN}})[0];
+        if (extensionCount + constructionCount < 5) {
+            let tries = 0;
+            let oks = 0;
+            for (let y = -2; y <= 2 && oks < 5; y += 2) {
+                for (let x = -2; x <= 2 && oks < 5; x += 2) {
+                    let px = spawn.pos.x + x;
+                    let py = spawn.pos.y + y;
+                    let result = room.createConstructionSite(px, py, STRUCTURE_EXTENSION);
+                    if (result === OK) {
+                        oks++;
+                    }
+                }
+            }
+        } 
+    },
+
+    spawnBuilders(room) {
+        let spawns = room.find(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_SPAWN, my:true}});
+        let sources = room.find(FIND_SOURCES);
+        let creeps = room.find(FIND_CREEPS, {filter: {my:true}});
+        let freePlains = _.sum(_.map(_.filter(sources, s => s.pos.findInRange(FIND_HOSTILE_CREEPS, 5).length === 0), s => s.plains.length));
+        let result = 0;
+        for(let spawnIndex in spawns) {
+            let spawn = spawns[spawnIndex];
+            if (countCreeps(creeps) < Math.floor(freePlains) && room.energyAvailable >= 250) {
+                console.log("spawning worker");
+                result = spawn.spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], "Worker MK2 " + Game.time);
+                console.log(result);
                 if (result === OK) {
-                    oks++;
+                    return result;
                 }
             }
         }
-    } 
-}
+        return result;
+    },
 
-function spawnBuilders(room) {
-    // let spawns = room.find(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_SPAWN, my:true}});
-    // let creeps = room.find(FIND_CREEPS, {filter: {my:true}});
-
-    // for(let spawnIndex in spawns) {
-    //     let spawn = spawns[spawnIndex];
-    //     if (countCreeps(creeps, "builder") < MaxBuilders && room.energyAvailable >= 300) {
-    //         spawn.spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], "Builder" + Game.time, {memory:{role:"builder"}});
-    //     }
-    // }
-}
-
-let level2 = {
     execute(room) {
 
-        buildExtensions(room);
-        spawnBuilders(room);
+        this.buildExtensions(room);
 
-        Strategies.room.level1.execute(room);
+        if (room.energyCapacityAvailable >= 500) {
+            if (this.spawnBuilders(room) !== OK) {
+                lvl1.spawnCreeps(room);
+            }
+        } else {
+            lvl1.spawnCreeps(room);
+        }
+
+        lvl1.ensureRoads(room);
+
     }
 }
 
